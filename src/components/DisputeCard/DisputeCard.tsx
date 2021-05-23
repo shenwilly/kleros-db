c8acc2ef0350b08ea365f6c77f5bfd76ce2a64c9
willy <shen.willy95@gmail.com>
2021-05-23 21:08:21 +0700

    update dispute card

--- src/components/DisputeCard/DisputeCard.tsx
+++ src/components/DisputeCard/DisputeCard.tsx
@@ -8,14 +8,30 @@ const DisputeCard: React.FC = () => {
         <>
             <Padding>
                 <Card>
+                    <CardTopRow>
+                        <span className="f4 b">#711</span>
+                        <Spacer/>
+                        <span>Humanity Court</span>
+                    </CardTopRow>
+                    <CardBottomRow>
+                        <PeriodContainer>
+                            Evidence Period
+                        </PeriodContainer>
+                        <Spacer/>
+                        <div className="tr">
+                            <div>
+                                Next period in:
+                            </div>
+                            <div>
+                                3 Days
+                            </div>
+                        </div>
+                    </CardBottomRow>
                     <div>
-                        #711
+                        
                     </div>
                     <div>
-                        Humanity Court
-                    </div>
-                    <div>
-                        Evidence Period
+                        
                     </div>
                 </Card>
             </Padding>   
@@ -24,16 +40,34 @@ const DisputeCard: React.FC = () => {
 };
 
 const Padding = styled.div.attrs({
-    className: 'dib w-50 w-third-gt-xs ph3 pb3'
+    className: 'dib w-50 w-third-gt-xs ph3 pb4'
 })`
 
 `;
 
 const Card = styled.div.attrs({
-    className: 'dib br3 w-100 pa3 bg-white'
+    className: 'dib br3 w-100 pa3 bg-white pointer'
 })`
     -webkit-box-shadow: 1px 3px 8px 1px rgba(0,0,0,0.2); 
     box-shadow: 1px 3px 8px 1px rgba(0,0,0,0.15);
+
+    background-image:
+        radial-gradient(at top right, rgb(242,227,255), transparent),
+        radial-gradient(at bottom left, rgb(242,227,255), transparent);
+`;
+
+const CardTopRow = styled.div.attrs({
+    className: 'db w-100 flex nowrap'
+})``;
+
+const CardBottomRow = styled.div.attrs({
+    className: 'db w-100 flex nowrap items-end mt4'
+})``;
+
+const PeriodContainer = styled.div.attrs({
+    className: 'di bg-green white pv2 ph3 br4 b'
+})`
+    position: relative
 `;
 
 const HeaderGrid = styled.div.attrs({
