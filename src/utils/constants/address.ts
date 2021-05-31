7f6fd1af2dc8052a1755570439a3c72f68aaffbe
willy <shen.willy95@gmail.com>
2021-05-31 17:38:32 +0700

    add env

--- src/utils/constants/address.ts
+++ src/utils/constants/address.ts
@@ -1,4 +1,4 @@
 export const KLEROS_COURT_ADDRESS = "0x988b3a538b618c7a603e1c11ab82cd16dbe28069";
-export const INFURA_ENDPOINT = "https://mainnet.infura.io/v3/76e069e8df464e1ebfc1244368e2108b";
+export const PROVIDER_ENDPOINT = process.env.REACT_APP_PROVIDER_ENDPOINT;
 export const KLEROS_SUBGRAPH_ENDPOINT = "https://api.thegraph.com/subgraphs/name/shenwilly/kleros-liquid";
 export const POH_SUBGRAPH_ENDPOINT = "https://api.thegraph.com/subgraphs/name/kleros/proof-of-humanity-mainnet";
